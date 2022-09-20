3314d4b9c122f0df981160de9760be49dd088fd3
muhammedRezk <mrezk319@gmail.com>
2022-09-20 11:46:57 +0200

    done

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -1,6 +1,5 @@
 package com.udacity
 
-import android.animation.AnimatorInflater
 import android.animation.PropertyValuesHolder
 import android.animation.ValueAnimator
 import android.content.Context
@@ -8,7 +7,6 @@ import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
-import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -20,18 +18,50 @@ class LoadingButton @JvmOverloads constructor(
         textSize = 60.0f
         textAlign = Paint.Align.CENTER
     }
-    private val paintCircle = Paint().apply { color = Color.RED }
-    private val paintRec = Paint().apply { color = Color.DKGRAY }
+    private val drawACircle = Paint().apply { color = Color.RED }
+    private val drawARectangle = Paint().apply { color = Color.DKGRAY }
     private val Circle_PERCENTAGE_VALUE_HOLDER = "CirclePercentage"
     private val Rec_PERCENTAGE_VALUE_HOLDER = "RecPercentage"
-    private var circleState = 0f
-    private var recState = 0f
+    private var CState = 0f
     private var clicked = false
     private val background = myTheme.getColor(R.styleable.LoadingButton_backGround, 0)
+    private var recState = 0f
     private val textClicked = myTheme.getString(R.styleable.LoadingButton_textClicked)
     private val textNotClicked = myTheme.getString(R.styleable.LoadingButton_textNotClicked)
     private var text = textNotClicked
-    private fun animateProgress() {
+    override fun onDraw(can: Canvas) {
+        super.onDraw(can)
+        can.drawColor(background)
+        if (CState == 360f) {
+            clicked = false
+            text = textNotClicked
+            isClickable = true
+        }
+        if (clicked) {
+            can.drawRect(
+                0f,
+                0f,
+                recState,//////
+                height.toFloat(),
+                drawARectangle
+            )
+            val Xtc = ((width / 2) - ((paintText.descent() + paintText.ascent())) / 2) + 65 * 4
+            val Ytc = (height / 2).toFloat()
+            can.drawArc(
+                Xtc + 10 - 30, Ytc - 30,
+                Xtc + 40, Ytc + 30,
+                0F, CState,
+                true, drawACircle
+            )
+        }
+        can.drawText(
+            text!!,
+            ((width / 2) - ((paintText.descent() + paintText.ascent())) / 2),
+            ((height / 2) - ((paintText.descent() + paintText.ascent())) / 2),
+            paintText
+        )
+    }
+    override fun performClick(): Boolean {
         isClickable = false; clicked = true; text = textClicked
         val circleValue = PropertyValuesHolder.ofFloat(
             Circle_PERCENTAGE_VALUE_HOLDER,
@@ -48,47 +78,12 @@ class LoadingButton @JvmOverloads constructor(
             duration = 1000
             interpolator = AccelerateDecelerateInterpolator()
             addUpdateListener {
-                circleState = it.getAnimatedValue(Circle_PERCENTAGE_VALUE_HOLDER) as Float
+                CState = it.getAnimatedValue(Circle_PERCENTAGE_VALUE_HOLDER) as Float
                 recState = it.getAnimatedValue(Rec_PERCENTAGE_VALUE_HOLDER) as Float
                 invalidate()
             }
         }
         animator.start()
-    }
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        canvas.drawColor(background)
-        if (circleState == 360f) {
-            clicked = false
-            text = textNotClicked
-            isClickable = true
-        }
-        if (clicked) {
-            canvas.drawRect(
-                0f,
-                0f,
-                recState,//////
-                height.toFloat(),
-                paintRec
-            )
-            val Xtc = ((width / 2) - ((paintText.descent() + paintText.ascent())) / 2) + 65 * 4
-            val Ytc = (height / 2).toFloat()
-            canvas.drawArc(
-                Xtc + 10 - 30, Ytc - 30,
-                Xtc + 40, Ytc + 30,
-                0F, circleState,
-                true, paintCircle
-            )
-        }
-        canvas.drawText(
-            text!!,
-            ((width / 2) - ((paintText.descent() + paintText.ascent())) / 2),
-            ((height / 2) - ((paintText.descent() + paintText.ascent())) / 2),
-            paintText
-        )
-    }
-    override fun performClick(): Boolean {
-        animateProgress()
         return super.performClick()
     }
 
